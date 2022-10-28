bf8ebcb945a70a78c773e004c6bf75efbf3b7dab
Amirhossein <amirhosseinzangooiizadeh@gmail.com>
2022-10-28 15:52:32 +0330

    compeleted

--- snake-game.js
+++ snake-game.js
@@ -1,26 +1,55 @@
 const snakeHead = document.getElementById("snake-head");
-this.snakeLeftPosition = 0;
-this.snakeTopPosition = 0;
+this.snakeLeftPosition = 100;
+this.snakeTopPosition = 100;
+let lastTurn = "";
 
-const snakeMovingRight = () => {
+let snakeMovingRight = () => {
   this.snakeLeftPosition += 20;
   snakeHead.style.left = this.snakeLeftPosition.toString() + "px";
   eatingFood();
+  addPositon();
+  tailPosition();
+  lastTurn = "horizental";
+  if (Math.floor(window.innerWidth / 20) * 20 <= snakeHead.offsetLeft) {
+    youLost();
+  }
+  checkTail();
 };
-const snakeMovingLeft = () => {
+let snakeMovingLeft = () => {
   this.snakeLeftPosition += -20;
   snakeHead.style.left = this.snakeLeftPosition.toString() + "px";
   eatingFood();
+  addPositon();
+  tailPosition();
+  lastTurn = "horizental";
+  if (snakeHead.offsetLeft == 0) {
+    youLost();
+  }
+  checkTail();
 };
-const snakeMovingUP = () => {
+let snakeMovingUP = () => {
   this.snakeTopPosition += -20;
   snakeHead.style.top = this.snakeTopPosition.toString() + "px";
   eatingFood();
+  addPositon();
+  tailPosition();
+  lastTurn = "vertical";
+  if (snakeHead.offsetTop == 0) {
+    youLost();
+  }
+  checkTail();
 };
-const snakeMovingDown = () => {
+let snakeMovingDown = () => {
   this.snakeTopPosition += 20;
   snakeHead.style.top = this.snakeTopPosition.toString() + "px";
   eatingFood();
+  addPositon();
+  tailPosition();
+  lastTurn = "vertical";
+  if (Math.floor(window.innerHeight / 20) * 20 <= snakeHead.offsetTop) {
+    youLost();
+  }
+  checkTail();
 };
 let direction = "";
 const changeDirection = (event) => {
@@ -34,14 +63,29 @@ let interval = setInterval(function () {
 }, 500);
 
 const specifingDirection = () => {
-  clearInterval(interval);
-  if (direction == "ArrowRight") {
+  if (
+    direction == "ArrowRight" &&
+    (lastTurn === "vertical" || lastTurn === "")
+  ) {
+    clearInterval(interval);
     interval = setInterval(snakeMovingRight, 200);
-  } else if (direction == "ArrowLeft") {
+  } else if (
+    direction == "ArrowLeft" &&
+    (lastTurn === "vertical" || lastTurn === "")
+  ) {
+    clearInterval(interval);
     interval = setInterval(snakeMovingLeft, 200);
-  } else if (direction == "ArrowUp") {
+  } else if (
+    direction == "ArrowUp" &&
+    (lastTurn === "horizental" || lastTurn === "")
+  ) {
+    clearInterval(interval);
     interval = setInterval(snakeMovingUP, 200);
-  } else if (direction == "ArrowDown") {
+  } else if (
+    direction == "ArrowDown" &&
+    (lastTurn === "horizental" || lastTurn === "")
+  ) {
+    clearInterval(interval);
     interval = setInterval(snakeMovingDown, 200);
   }
 };
@@ -56,14 +100,19 @@ let food = document.getElementById("food");
 
 const changeFoodPos = () => {
   food.style.left =
-    (Math.floor((Math.random() * window.innerHeight) / 20) * 20).toString() +
-    "px";
+    (
+      Math.floor((Math.random() * (window.innerHeight - 60)) / 20) * 20 +
+      40
+    ).toString() + "px";
   food.style.top =
-    (Math.floor((Math.random() * window.innerHeight) / 20) * 20).toString() +
-    "px";
+    (
+      Math.floor((Math.random() * (window.innerHeight - 60)) / 20) * 20 +
+      40
+    ).toString() + "px";
 };
 
 changeFoodPos();
+let tally = 0;
 
 const eatingFood = () => {
   if (
@@ -71,5 +120,78 @@ const eatingFood = () => {
     food.style.top === snakeHead.style.top
   ) {
     changeFoodPos();
+    addLength();
+    tally++;
   }
 };
+
+const createElement = (tag, attributes) => {
+  const el = document.createElement(tag);
+  attributes = attributes || {};
+  for (let attr in attributes)
+    attributes.hasOwnProperty(attr) && el.setAttribute(attr, attributes[attr]);
+  return el;
+};
+
+let newTail = "new-tail";
+
+const addLength = () => {
+  const addTail = createElement("div", { class: newTail });
+  document.body.appendChild(addTail);
+};
+
+let positionArray = [];
+let positionObject = {};
+let x = 0;
+let y = 0;
+tail = document.getElementsByClassName("new-tail");
+
+const addPositon = () => {
+  positionObject = {
+    x: document.getElementById("snake-head").offsetLeft,
+    y: document.getElementById("snake-head").offsetTop,
+  };
+  positionArray.push(positionObject);
+};
+const tailPosition = () => {
+  for (let index = 0; index < tally; index++) {
+    document.getElementsByClassName("new-tail")[index].style.left =
+      positionArray[positionArray.length - (index + 2)].x.toString() + "px";
+
+    document.getElementsByClassName("new-tail")[index].style.top =
+      positionArray[positionArray.length - (index + 2)].y.toString() + "px";
+  }
+};
+
+setInterval(() => {
+  if (positionArray.length > 1000) {
+    positionArray.shift();
+  }
+}, 200);
+
+const youLost = () => {
+  clearInterval();
+  alert(`youLost
+  your score is: ${tally}`);
+  snakeMovingDown = {};
+  snakeMovingLeft = {};
+  snakeMovingRight = {};
+  snakeMovingUP = {};
+};
+let y2 = snakeHead.offsetTop;
+x2 = snakeHead.offsetLeft;
+
+const checkTail = () => {
+  for (let index = 0; index < tally; index++) {
+    y2 = snakeHead.offsetTop;
+    x2 = snakeHead.offsetLeft;
+    if (
+      positionArray[positionArray.length - (index + 2)].x === x2 &&
+      positionArray[positionArray.length - (index + 2)].y === y2
+    ) {
+      youLost();
+    }
+  }
+};
+
+let tailArray = document.getElementsByClassName("new-tail");
